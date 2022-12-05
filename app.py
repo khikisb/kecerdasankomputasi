700cf26cdecf0cf1d7df4fec4dd79bdca258ecc2
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-05 17:20:08 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pickle
+import streamlit as st
+
+st.set_page_config(
+    page_title="Prediksi Jumlah Pasien Diabetes",
+    page_icon="👋",
+)
+
+st.title("")
+st.sidebar.success("Pilih Halaman Yang Ingin Anda Tuju.")
+
+
+st.title("Prediksi Jumlah Pasien Diabetes Di RSUD Syarifah Ambami Ratu Ebu")
+
+    
+st.header('Jawablah Semua Pertanyaan Berikut :')
