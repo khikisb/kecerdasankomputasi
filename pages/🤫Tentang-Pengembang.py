b3631987dcff9d21f776e3488256843799bcd461
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-05 17:38:34 +0700

    Update 🤫Tentang-Pengembang.py

--- pages/🤫Tentang-Pengembang.py
+++ pages/🤫Tentang-Pengembang.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Code-Project",
+    page_title="Tentang-Pengembang",
     page_icon="👋",
 )
 
 st.title("Halo Semuanya !👋")
+st.title("Kami dari Kelompok 2")
+st.title("Yang Terdiri dari 3 Mahasiswa")
 
 tab1, tab2, tab3 = st.tabs(["Okhi Sahrul Barkah", "Farid Ghozali", "Afirza Lucky Pradana"])
 
