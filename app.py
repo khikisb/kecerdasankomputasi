ce03bde3fe00bdf341afde51218befecef60d662
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-06 15:14:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,4 @@ with tab4:
         df2 = pd.DataFrame(databaruu2)
         
         dataset_total = pd.concat((df1, df2), axis=0)
-        inputs = dataset_total[len(dataset_total)-len(df2) - timesteps:].values.reshape(-1,1)
-        inputs = scaler.transform(inputs) #minmax scaler
-        inputs
+        len(dataset_total)
