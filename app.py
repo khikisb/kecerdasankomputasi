9d14d86af874df78badd28370f7f7d5edb988144
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-06 11:20:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ with tab4:
         
         temp1=[]
         for i in df1['Usia']:
-            temp1.append(i)
+            temp.append(i)
     
         databaru2 = [temp,temp1]
         databaru2
