baf01047f02042e9ea5cc466cd3b93c7ed9eb128
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-08 09:38:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,16 @@ with tab2:
     st.title("Pre-Processing")
     #Observation units for variables with a minimum value of zero are NaN, except for the pregnancy variable.
     df.describe([0.05,0.25,0.50,0.75,0.90,0.95,0.99]).T
+    
+    # NaN values of 0 for Glucose, Blood Pressure, Skin Thickness, Insulin, BMI
+    # We can write Nan instead of 0
+    cols = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
+    for col in cols:
+        df[col].replace(0,np.NaN,inplace=True)
+     
+    # now we can see missing values
+    st.write("Missing Value")
+    df.isnull().sum()
 
     
 with tab3:
