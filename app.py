bd4805b7d99c824424fbb2d5682c68f98ae6bfde
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-09 01:42:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ with tab4:
         prediksi = predict(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
         st.success(f'Anda Di Prediksi {prediksi}')
         #if prediksi != 0:
-            prediksi1 = "Diabetes"
-            st.success(f'Anda Di Prediksi {prediksi1}')
+            #prediksi1 = "Diabetes"
+            #st.success(f'Anda Di Prediksi {prediksi1}')
         #else:
-            prediksi2 = "Tidak Diabetes"
-            st.success(f'Anda Di Prediksi {prediksi2}')
+            #prediksi2 = "Tidak Diabetes"
+            #st.success(f'Anda Di Prediksi {prediksi2}')
