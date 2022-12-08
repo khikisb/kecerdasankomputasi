011181698e1044e910dbef46025ea7b0a638e46e
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-08 18:44:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
+import pickle
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 st.set_page_config(
     page_title="Prediksi Diabetes",
@@ -36,6 +35,45 @@ with tab2:
     # now we can see missing values
     st.write("Missing Value")
     st.image("MissingValue.png")
+    
+    # We can fill in NaN values with a median according to the target
+    for col in df.columns:
+        df.loc[(df["Outcome"]==0) & (df[col].isnull()),col] = df[df["Outcome"]==0][col].median()
+        df.loc[(df["Outcome"]==1) & (df[col].isnull()),col] = df[df["Outcome"]==1][col].median()
+    
+    df.isnull().sum()
+    st.image("missingvalueupdate.png")
+    
+    def outlier_thresholds(dataframe, variable):
+        quartile1 = dataframe[variable].quantile(0.10)
+        quartile3 = dataframe[variable].quantile(0.90)
+        interquantile_range = quartile3 - quartile1
+        up_limit = quartile3 + 1.5 * interquantile_range
+        low_limit = quartile1 - 1.5 * interquantile_range
+        return low_limit, up_limit
+    
+    def has_outliers(dataframe, variable):
+        low_limit, up_limit = outlier_thresholds(dataframe, variable)
+        if dataframe[(dataframe[variable] < low_limit) | (dataframe[variable] > up_limit)].any(axis=None):
+            print(variable, "yes")
+            
+    for col in df.columns:
+        has_outliers(df, col)
+        
+    def replace_with_thresholds(dataframe, numeric_columns):
+        for variable in numeric_columns:
+            low_limit, up_limit = outlier_thresholds(dataframe, variable)
+            dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+            dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+            
+    replace_with_thresholds(df, df.columns)
+    
+    for col in df.columns:
+        has_outliers(df, col)
+    
+    df.describe([0.05,0.25,0.50,0.75,0.90,0.95,0.99]).T
+    
+    st.image("describe.png")
 
     
 with tab3:
