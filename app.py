51098252fddbe17b819dc417f8a908ac98c4ad44
khikisb <89850965+khikisb@users.noreply.github.com>
2022-12-12 13:24:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,23 +15,17 @@ st.title("")
 st.sidebar.success("Pilih Halaman Yang Ingin Anda Tuju.")
 
 
-st.title("Prediksi Diabets")
+st.title("Prediksi Diabetes")
 
     
-tab1, tab2, tab3, tab4 = st.tabs(["Deskripsi Data", "Tab Pre-Processing", "Tab Modeling", "Tab Implementasi"])
+tab1, tab2 = st.tabs(["Deskripsi Data", "Tab Implementasi"])
 
 with tab1:
     df =pd.read_csv("diabetes.csv")
 
     st.table(df)
-
+    
 with tab2:
-    st.title("Pre-Processing")
-    
-with tab3:
-    st.write("Salahh bro")
-    
-with tab4:
     st.write("Salahh bro")
 
     # Define the prediction function
